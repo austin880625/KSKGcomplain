3f5517eb1a4f725e53e3c307796e87c0466d2a62
Austin Chang <austin880625@gmail.com>
2016-02-21 01:39:10 +0800

    modifoed wsgi 10

--- wsgi.py
+++ wsgi.py
@@ -5,10 +5,10 @@ from cherrypy import wsgiserver
 
 from kskgcomplain.wsgi import application
 
-ip = os.environ['OPENSHIFT_PYTHON_IP']
-port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-host_name = os.environ['OPENSHIFT_GEAR_DNS']
+#ip = os.environ['OPENSHIFT_PYTHON_IP']
+#port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+#host_name = os.environ['OPENSHIFT_GEAR_DNS']
 
 
-server = wsgiserver.CherryPyWSGIServer((ip, port), application, server_name=host_name)
-server.start()
+#server = wsgiserver.CherryPyWSGIServer((ip, port), application, server_name=host_name)
+#server.start()
