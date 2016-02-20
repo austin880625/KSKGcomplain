144a63f8d2f8c112b919e320c2aafaf0dd4ba646
Austin Chang <austin880625@gmail.com>
2016-02-21 00:55:24 +0800

    modified wsgi 7

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
-#!/bin/bash
-~/python/virtenv/venv/bin/activate
-gunicorn kskgcomplain.wsgi 127.0.0.1:8071
+#!/usr/bin/python
+import os
+import sys
+
+virtenv=os.environ['OPENSHIFT_PYTHON_DIR']+'/virtenv/venv/bin/activate'
+os.system(virtenv)
+ip = os.environ['OPENSHIFT_PYTHON_IP']
+port = os.environ['OPENSHIFT_PYTHON_PORT']
+host_name = os.environ['OPENSHIFT_GEAR_DNS']
+os.system('gunicorn kskgcomplain.wsgi localhost:'+port)
