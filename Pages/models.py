29cd3e9220a8b7ac6735b6191c1f41edc985394f
Austin Chang <austin880625@gmail.com>
2016-03-13 09:34:20 +0800

    changed banner into base64 format

--- Pages/models.py
+++ Pages/models.py
@@ -5,5 +5,5 @@ class Page(models.Model):
     page_id=models.CharField(max_length=100)
     access_token=models.CharField(max_length=300)
     prefix=models.CharField(max_length=100)
-    placehold=models.CharField(max_length=30)
+    placehold=models.CharField(max_length=30,default='YEE?')
     post_count=models.IntegerField()
