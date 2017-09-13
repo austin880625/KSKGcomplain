773be8527b32e2a5252da2cd913545799864a20d
Austin Chang <austin880625@gmail.com>
2017-09-13 23:23:01 +0800

    changed wsgi.py

--- kskgcomplain/settings.py
+++ kskgcomplain/settings.py
@@ -15,10 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DB_NAME = os.environ['DB_NAME']
-DB_USER = os.environ['DB_USER']
-DB_PASS = os.environ['DB_PASS']
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
